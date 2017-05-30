24e65030e1732f13f9b0a3079bf076ac01275c9e
Caleb Chapman <cchapman0069@kctcs.edu>
2017-05-30 09:25:03 -0400

    cookies again

--- scripts/main.js
+++ scripts/main.js
@@ -31,19 +31,15 @@ function setCookie(cname, cvalue) {
     document.cookie = cname + "=" + cvalue + ";" + ";path=/";
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
+function readCookie(name) {
+    var nameEQ = name + "=";
     var ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
-    return "0";
+    return 0;
 }
 
 function buttonColor() {
