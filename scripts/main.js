59dc2d55ac71d4b61b949aff44f1bb4db8086dc2
Caleb Chapman <cchapman0069@kctcs.edu>
2017-05-30 00:50:15 -0400

    Added Cookies??

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,50 @@ var owned = {
     , Benji: 0
     , MM4005: 0
 }
-console.log(owned.CreativeSushi, owned.sam)
 multiplier = 1;
 amount = 0;
 totalPixels = 0;
+amount = start('amount');
+totalPixels = start('pixels');
+owned = start('owned');
+if (amount === "") {
+    amount = 0;
+}
+if (totalPixels === "") {
+    totalPixels = 0;
+}
+if (owned === "") {
+    owned = {
+        CreativeSushi: 0
+        , sam: 0
+        , StarLord: 0
+        , WeatherGeek: 0
+        , Benji: 0
+        , MM4005: 0
+    }
+}
 
 function timer() {
     totalPixels = totalPixels + (amount * multiplier / 100);
     updateGUI();
+}
+
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";" + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
--- scripts/updateGUI.js
+++ scripts/updateGUI.js
@@ -1,5 +1,8 @@
 function updateGUI() {
     document.getElementById("pixels").innerHTML = String(totalPixels.toFixed(0));
+    setCookie('amount', amount);
+    setCookie('pixels', totalPixels);
+    setCookie('own', owned);
 }
 
 function updateOwnedGUI(item) {
